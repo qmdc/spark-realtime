2ca53037e9e93ac672a1dc060fd4acb96ed59b35
千岛 <notre1024@163.com>
2022-11-25 10:50:35 +0800

    ES检索数据完成✅项目完成✅

---
+++ publisher-realtime/src/main/java/com/qiandao/publisherrealtime/PublisherRealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.qiandao.publisherrealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherRealtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublisherRealtimeApplication.class, args);
+    }
+
+}
---
+++ publisher-realtime/src/test/java/com/qiandao/publisherrealtime/PublisherRealtimeApplicationTests.java
@@ -0,0 +1,21 @@
+package com.qiandao.publisherrealtime;
+
+import com.qiandao.publisherrealtime.mapper.impl.PublisherMapperImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+@SpringBootTest
+class PublisherRealtimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // PublisherMapperImpl publisherMapper = new PublisherMapperImpl();
+        // Map<String, Long> stringLongMap = publisherMapper.searchDauHr("gmall_dau_info_" + "2022-11-24");
+        // stringLongMap.forEach(System.out::printf);
+    }
+
+
+
+}
